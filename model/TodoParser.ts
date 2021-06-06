ba715f6ba67839120eca373a0fe321f443251789
frenchtoasters <5040983+frenchtoasters@users.noreply.github.com>
2021-06-06 00:14:13 -0400

    Update TodoParser.ts

--- model/TodoParser.ts
+++ model/TodoParser.ts
@@ -12,8 +12,7 @@ export class TodoParser {
     const description = entry[3];
 
     const datePattern = /#(\d{4}-\d{2}-\d{2})/g;
-    const somedayPattern = /#(someday)/g;
-    const trackingPattern = /#(track)/g;
+    const somedayPattern = /#(someday)|#(track)/g;
     const dateMatches = description.match(datePattern);
     const actionDate = dateMatches != null ? new Date(dateMatches[0]?.substring(1)) : undefined;
 
